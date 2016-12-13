1e0fc345d1bbedead8015fade10435168c235fff
swedishbex <beckis@rocketmail.com>
2016-12-13 14:16:18 +0100

    new files - Park Name
    Pages for info about each Park

---
+++ ParkName.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace fakie
+{
+	public partial class ParkName : ContentPage
+	{
+		public ParkName()
+		{
+			InitializeComponent();
+		}
+	}
+}
