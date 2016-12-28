f1a16bba80db0d04625cd1ab1a9883fec87636c4
Mikael Borgbrant <mikael.borgbrant@gmail.com>
2016-12-28 15:23:31 +0100

    Fixed stack layout to lan page

---
+++ fakie/RegionIndex.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace fakie
+{
+	public partial class RegionIndex : ContentPage
+	{
+		public RegionIndex()
+		{
+			InitializeComponent();
+		}
+	}
+}
